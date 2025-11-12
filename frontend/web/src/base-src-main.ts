128dde92848eac8199de5a494eb6631b866412ae
ightmon <ightmon@users.noreply.github.com>
2025-11-12 17:32:20 +0000

    Add base-src-main.ts, complex-api-mutations.ts

---
+++ frontend/web/src/base-src-main.ts
@@ -0,0 +1,5 @@
+import { ethers } from 'ethers';
+
+export default function customer_payment() { return <div>Component</div>; }
+
+
---
+++ frontend/web/src/complex-api-mutations.ts
@@ -0,0 +1,5 @@
+import React from 'react';
+
+interface gateway_components_commonProps { data: string }
+
+
