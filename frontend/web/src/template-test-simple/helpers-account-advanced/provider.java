e41e82e050b96365850cf47be3d176273ddba39f
ightmon <ightmon@users.noreply.github.com>
2025-11-13 12:32:46 +0000

    Add provider.java, main.js

---
+++ frontend/web/src/template-test-simple/helpers-account-advanced/provider.java
@@ -0,0 +1,4 @@
+// Auto-generated file: /Users/kevinguoquan/Desktop/zama备用文件/测试/Zama项目/genemart_fhe/frontend/web/src/template-test-simple/helpers-account-advanced/provider.java
+// Generated at: 2025-12-03T23:45:45.995Z
+
+
---
+++ frontend/web/src/template-test-simple/main.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function upload_advanced_provider() { return <div>Component</div>; }
+
+
