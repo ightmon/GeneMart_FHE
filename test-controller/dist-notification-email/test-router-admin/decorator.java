613e55950c37087f5adfad1f671c7c2c242a3401
ightmon <ightmon@users.noreply.github.com>
2025-11-14 15:48:09 +0000

    Add decorator.java

---
+++ test-controller/dist-notification-email/test-router-admin/decorator.java
@@ -0,0 +1,4 @@
+// Auto-generated file: /Users/kevinguoquan/Desktop/zama备用文件/测试/Zama项目/genemart_fhe/test-controller/dist-notification-email/test-router-admin/decorator.java
+// Generated at: 2025-12-03T23:45:45.993Z
+
+
